fca64cd873d2eb83de1a8d6dae651557f02f1f25
Tom Darcy <tdarcytom@googlemail.com>
2023-02-22 12:13:23 +0800

    index done

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,49 @@
+class FlatsController < ApplicationController
+  def index
+    @flats = if params[:search]
+               Flat.where('name LIKE ? OR address LIKE ?', "%#{params[:search]}%", "%#{params[:search]}%")
+             else
+               Flat.all
+             end
+    @flats_quantity = @flats.count
+  end
+
+  def show
+    @flat = Flat.find(params[:id])
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to @flat
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
+  end
+
+  private
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :image_url)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Flat.destroy_all
+
+10.times do
+  Flat.create!(
+    name: Faker::Address.community,
+    address: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price_per_night: rand(50..200),
+    number_of_guests: rand(1..6),
+    # create a random image from unsplash using the flat name as a keyword
+    image_url: "https://source.unsplash.com/1600x900/?#{Faker::Address.community}"
+  )
+  puts 'Flat created'
+end
